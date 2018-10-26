71047d3e811f1378e3453c69cb39e0c530bfc725
Patricia Feio <patriciafeio@gmail.com>
2018-10-26 17:09:44 +0100

    Added more exercises

---
+++ 5-Basic-Data-Structures/012-create-complex-multi-dimensional-arrays.js
@@ -0,0 +1,22 @@
+let myNestedArray = [
+  // change code below this line
+  [
+  ['unshift', false, 1, 2, 3, 'complex', 'nested'],['loop', 'shift', 6, 7, 1000]
+  ],
+    [
+       ['concat', false, true, 'spread', 'deep'],
+    ],
+    [
+      [
+        ['mutate', 1327.98, 'splice', 'slice', 'deeper'],
+      ]
+    ],
+    [
+      [
+        [
+          ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'deepest']
+        ]
+      ]
+    ]
+  // change code above this line
+];
