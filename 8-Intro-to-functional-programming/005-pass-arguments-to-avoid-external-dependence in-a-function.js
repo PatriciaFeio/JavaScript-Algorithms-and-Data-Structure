2ec1e4b379887497c4f32b284032dc92e717ce1b
Patricia Feio <patriciafeio@gmail.com>
2018-11-23 00:39:29 +0000

    Added Intro to Functional Programming file with exercises 1 to 6

---
+++ 8-Intro-to-functional-programming/005-pass-arguments-to-avoid-external-dependence in-a-function.js
@@ -0,0 +1,16 @@
+// the global variable
+var fixedValue = 4;
+
+// Add your code below this line
+function incrementer (fixedValue) {
+
+  var fixedValue = 4;
+
+  return fixedValue + 1;
+  
+  
+  // Add your code above this line
+}
+
+var newValue = incrementer(fixedValue); // Should equal 5
+console.log(fixedValue); // Should print 4
