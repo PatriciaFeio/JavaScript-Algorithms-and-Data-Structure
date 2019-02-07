c4332a1c07a7a411f5c96b7139afbf66203198dd
Patricia Feio <patriciafeio@gmail.com>
2019-02-07 01:31:56 +0000

    Added exercise 12

---
+++ 8-Intro-to-functional-programming/012-remove-elements-from-an-array-using-slice-instead-of-splice.js
@@ -0,0 +1,13 @@
+function nonMutatingSplice(cities) {
+  // Add your code below this line
+  //return cities.splice(3);
+
+  return cities.slice(0, 3);
+  
+  // Add your code above this line
+}
+var inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+nonMutatingSplice(inputCities);
+
+
+
