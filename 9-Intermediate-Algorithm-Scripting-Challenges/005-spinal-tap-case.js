eeb446a065710954996b80e7f81d9885c30ab4a4
Patricia Feio <patriciafeio@gmail.com>
2019-07-11 12:25:00 +0100

    Added exercise number 5

---
+++ 9-Intermediate-Algorithm-Scripting-Challenges/005-spinal-tap-case.js
@@ -0,0 +1,11 @@
+function spinalCase(str) {
+    // "It's such a fine line between stupid, and clever."
+    // --David St. Hubbins
+    const reg = /\s+|_+/g
+
+    str = str.replace(/([a-z])([A-Z])/g, '$1 $2')
+
+    return str.replace(reg, '-').toLowerCase()
+}
+
+spinalCase('This Is Spinal Tap');
