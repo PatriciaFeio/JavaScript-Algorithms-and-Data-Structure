5387a09eaeb58c6493d632943b2d98df72e03ac4
Patricia Feio <patriciafeio@gmail.com>
2019-07-11 01:37:09 +0100

    Added thirs solved exercise

--- 9-Intermediate-Algorithm-Scripting-Challenges/003-seek-and-destroy.js
+++ 9-Intermediate-Algorithm-Scripting-Challenges/003-seek-and-destroy.js
@@ -1,6 +1,15 @@
 function destroyer(arr) {
   // Remove all the values
-  return arr;
+  let args = Array.prototype.slice.call(arguments);
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < args.length; j++) {
+      if (arr[i] === args[j]) {
+        delete arr[i];
+      }
+    }
+  }
+  return arr.filter(Boolean);
 }
 
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
