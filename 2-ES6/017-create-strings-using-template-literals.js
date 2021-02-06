36c7093e7c85fb90be0011cd76c523c4774d2c66
Patrícia Feio <patriciafeio@gmail.com>
2021-02-06 04:11:15 +0000

    change file format and add starter files

---
+++ 2-ES6/017-create-strings-using-template-literals.js
@@ -0,0 +1,36 @@
+// Create Strings using Template Literals
+
+/* const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    // Only change code below this line
+    const failureItems = [];
+    // Only change code above this line
+  
+    return failureItems;
+  }
+  
+  const failuresList = makeList(result.failure); */
+
+  const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    // Only change code below this line
+      const failureItems = [];
+  
+      for (const value of arr) {
+         failureItems.push(`<li class="text-warning">${value}</li>`)
+       }
+    
+    // Only change code above this line
+  
+    return failureItems;
+  }
+  
+  const failuresList = makeList(result.failure);
---
+++ 2-ES6/018-write-concise-object-literal-declarations-using-object-property-shorthand.js
@@ -0,0 +1,21 @@
+// Write Concise Object Literal Declarations Using Object Property Shorthand
+
+/* const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return {
+      name: name,
+      age: age,
+      gender: gender
+    };
+    // Only change code above this line
+  }; */
+
+  const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return {
+      name,
+      age,
+      gender
+    };
+    // Only change code above this line
+  };
